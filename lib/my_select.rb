aeb98de781a5da0f55fb05ee587801c0505b26cb
Rosa Tu <rosalina.tu@gmail.com>
2018-07-08 23:18:29 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
-end
+    i = 0
+   new_collection = []
+   while i < collection.length
+   if 
+     yield(collection[i]) != nil 
+   new_collection << collection[i]
+  end
+  i = i + 1
+  end
+  new_collection
+end  
+
+
+#i need to return a new collection of elements that have been evaluated true.
